450b1e069daeb8c1a856641f2b71194db4979f32
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-10-10 22:44:15 -0700

    Initial version

---
+++ api/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongo = require("mongodb").MongoClient;
+
+const app = express();
+
+const url = `mongodb://${process.env.MONGODB_USERNAME}:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@${process.env.MONGODB_HOST}:27017/${process.env.MONGODB_DATABASE}`;
+
+function startWithRetry() {
+  mongo.connect(url, { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    reconnectTries: 30,
+    reconnectInterval: 1000
+  }, (err, client) => {
+    if (err) {
+      console.error(`Error connecting, retrying in 1 sec: ${err}`);
+      setTimeout(startWithRetry, 1000);
+      return;
+    }
+
+    const db = client.db(process.env.MONGODB_DATABASE);
+
+    app.listen(8080, () => {
+      app.get("/api/healthz", (req, res, next) => {
+        res.sendStatus(200)
+        return;
+      });
+
+      app.get("/api/movies", (req, res, next) => {
+        db.collection('movies').find().toArray( (err, results) =>{
+          if (err){
+            console.log(`failed to query movies: ${err}`)
+            res.json([]);
+            return;
+          }
+          res.json(results);
+        });
+      });
+
+      app.get("/api/mylist", (req, res, next) => {
+        db.collection('mylist').find().toArray( (err, results) =>{
+          if (err){
+            console.log(`failed to query movies: ${err}`)
+            res.json([]);
+            return;
+          }
+
+          res.json(results);
+        });
+      });
+
+      app.get("/api/watching", (req, res, next) => {
+        db.collection('watching').find().toArray( (err, results) =>{
+          if (err){
+            console.log(`failed to query movies: ${err}`)
+            res.json([]);
+            return;
+          }
+
+          res.json(results);
+        });
+      });
+
+      console.log("Server running on port 8080.");
+    });
+  });
+};
+
+startWithRetry();
