9af4e8677a9f0b8fe044e1a740af8ed22588e826
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-10-11 00:50:22 -0700

    move job into api repo

---
+++ api/load.js
@@ -0,0 +1,55 @@
+const mongo = require("mongodb").MongoClient;
+
+const url = `mongodb://${process.env.MONGODB_USERNAME}:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@${process.env.MONGODB_HOST}:27017/${process.env.MONGODB_DATABASE}`;
+
+var insert = function(collection, data, resolve, reject) {
+  const d = require(data);
+  d.results.forEach((doc) => {
+    doc._id = doc.id;
+  });
+  collection.insertMany(d.results, (err, r) => {
+    if (err) {
+      if (err.code != 11000) {
+        return reject(err);
+      }
+    }
+
+    resolve();
+  });
+}
+
+mongo.connect(url, { 
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  reconnectTries: 30,
+  reconnectInterval: 1000
+}, (err, client) => {
+  if (err) {
+    console.error(`Error connecting to mongodb, retrying in 1 sec: ${err}`);
+    process.exit(1);
+  }
+
+  var promises = [];
+  db = client.db(process.env.MONGODB_DATABASE);
+  promises.push(new Promise((resolve, reject)=>{
+    insert(db.collection('movies'), "./data/movies.json", resolve, reject);
+  }));
+
+  promises.push(new Promise((resolve, reject)=>{
+    insert(db.collection('mylist'), "./data/mylist.json", resolve, reject);
+  }));
+
+  promises.push(new Promise((resolve, reject)=>{
+    insert(db.collection('watching'), "./data/watching.json", resolve, reject);
+  }));
+
+  Promise.all(promises)
+  .then(function() { 
+    console.log('all loaded'); 
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(`fail to load: ${err}`);
+    process.exit(1);
+  });
+});
