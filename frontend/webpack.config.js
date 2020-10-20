ca314bdfec9c0f9b8cd50c00dda2dfedd62f0ee4
Ramon Lamana <rlamana@gmail.com>
2020-10-20 19:48:32 +0200

    Fix favicon

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -44,7 +44,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      favicon: path.resolve(__dirname, 'src/assets/images/favicon.png')
     })
   ],
   devServer: {
