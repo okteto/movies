e6117e27b05b0a3c28153ec54e22597d4b1df016
Pablo Chico de Guzman <pchico83@gmail.com>
2021-04-29 12:14:44 +0200

    Fix watching query

--- api/server.js
+++ api/server.js
@@ -40,7 +40,7 @@ function startWithRetry() {
 
       app.get("/api/watching", (req, res, next) => {
         console.log(`GET /api/watching`)
-        db.collection('movies').find().toArray( (err, results) =>{
+        db.collection('watching').find().toArray( (err, results) =>{
           if (err){
             console.log(`failed to query watching: ${err}`)
             res.json([]);
