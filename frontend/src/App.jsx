fbe52f7d315bc9da4874b323535f0fb20ae495a6
Ramón Lamana <rlamana@gmail.com>
2022-04-24 15:33:57 +0200

    Movie rentals UI update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-import userAvatarImage from './assets/images/user.jpg';
-import movieBackground from './assets/images/movie-bg.jpg';
+import userAvatarImage from './assets/images/cindy.jpg';
 
 import './App.css';
 
@@ -38,16 +37,11 @@ class App extends Component {
       fixHeader: false
     };
 
-    this.onScroll = this.onScroll.bind(this);
+    this.appRef = React.createRef();
   }
 
   componentDidMount() {
     this.refreshData();
-    window.addEventListener('scroll', this.onScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll);
   }
 
   handleRent = async (item) => {
@@ -90,40 +84,37 @@ class App extends Component {
     });
   }
 
-  onScroll = () => {
+  handleScroll = () => {
     this.setState({
-      fixHeader: window.scrollY > 100
+      fixHeader: this.appRef.current.scrollTop > 20
     });
   }
 
   render() {
     const { catalog, rental, session, cost } = this.state;
     return (
-      <div className="App">
-        <header className={`Header ${this.state.fixHeader ? 'fixed' : ''}`}>
-          <div className="content">
-            <div className="logo">Movies</div>
-            <ul className="menu">
-              <li className="selected">Home</li>
-              <li>Store</li>
-              <li>Movies</li>
-            </ul>
-            <UserProfile user={session} />
+      <div className="App" ref={this.appRef} onScroll={this.handleScroll}>
+        <header className={`App__header ${this.state.fixHeader ? 'fixed' : ''}`}>
+          <div className="App__logo">
+            <MoviesIcon />
+            Movies
           </div>
+          <UserProfile user={session} />
         </header>
-        <Hero />
-        <TitleList
-          title="Store"
-          titles={catalog.data}
-          loaded={catalog.loaded}
-          onRent={this.handleRent}
-        />
-        <TitleList
-          title={`${session.name}'s movies`}
-          cost={cost}
-          titles={rental.data}
-          loaded={rental.loaded}
-        />
+        <div className="App__content">
+          <TitleList
+            title="Store"
+            titles={catalog.data}
+            loaded={catalog.loaded}
+            onRent={this.handleRent}
+          />
+          <TitleList
+            title={`${session.name}'s movies`}
+            cost={cost}
+            titles={rental.data}
+            loaded={rental.loaded}
+          />
+        </div>
       </div>
     );
   }
@@ -171,43 +162,6 @@ class UserProfile extends Component {
   }
 }
 
-
-class Hero extends Component {
-  render() {
-    return (
-      <div id="hero" className="Hero" style={{ backgroundImage: `url(${movieBackground})` }}>
-        <div className="spring" />
-        <div className="content">
-          <h1>Bohemian Rhapsody</h1>
-          <p>
-            Queen take the music world by storm when they form the rock &apos;n&apos; roll
-            band in 1970.
-          </p>
-          <div className="button-container">
-            <HeroButton class="play-button">
-              <svg className="icon play-icon" width="20" height="20" viewBox="0 0 512 512">
-                <path d="M405.2 232.9L126.8 67.2c-3.4-2-6.9-3.2-10.9-3.2-10.9 0-19.8 9-19.8 20H96v344h.1c0 11 8.9 20 19.8 20 4.1 0 7.5-1.4 11.2-3.4l278.1-165.5c6.6-5.5 10.8-13.8 10.8-23.1s-4.2-17.5-10.8-23.1z"/>
-              </svg>
-              Play
-            </HeroButton>
-          </div>
-        </div>
-        <div className="overlay"></div>
-      </div>
-    );
-  }
-}
-
-
-class HeroButton extends Component {
-  render() {
-    return (
-      <a href="#" className="Button">{this.props.children}</a>
-    );
-  }
-}
-
-
 class TitleList extends Component {
   renderList() {
     const { titles = [], loaded, onRent } = this.props;
@@ -217,19 +171,13 @@ class TitleList extends Component {
       if (movies.length === 0) {
         return (
           <div className="TitleListEmpty">
-            {onRent ? 'No movies left to rent.' : 'No rented movies.'}
+            {onRent && 'No movies left to rent.'}
           </div>
         );
       }
 
       return movies.map((item, i) => {
-        let name = '';
         const backDrop = `https://image.tmdb.org/t/p/original${item.backdrop_path}`;
-        if (!item.name) {
-          name = item.original_title;
-        } else {
-          name = item.name;
-        }
         return (
           <Item
             key={item.id}
@@ -243,20 +191,18 @@ class TitleList extends Component {
   }
 
   render() {
-    const { title, cost = 0 } = this.props;
+    const { titles, title, cost = 0 } = this.props;
 
     return (
       <div className="TitleList">
         <div className="Title">
           <h1>
             {title}
-            {cost ?
-              <span className="TitleList__cost">
-                (Total paid: <strong>{`\$${cost}`}</strong>)
-              </span> : null
-            }
           </h1>
-          <div className="titles-slider">
+          <div className="TitleList__slider">
+            {!!cost &&
+              <Cart cost={cost} titles={titles} />
+            }
             {this.renderList() || <Loader />}
           </div>
         </div>
@@ -265,29 +211,76 @@ class TitleList extends Component {
   }
 }
 
-
-class Item extends Component {
-  render() {
-    const { item, onRent, backdrop } = this.props;
-
-    return (
-      <div className="Item">
-        <div className="ItemContainer" style={{ backgroundImage: `url(${backdrop})` }}>
-          <div className="overlay">
-            <div className="title">{item?.original_title ?? 'Unknown Title'}</div>
-            <div className="rating">{item?.vote_average ?? 0} / 10</div>
-          </div>
-          { onRent &&
-            <div className="ItemToolbar">
-              <div className="button button-rent" onClick={() => onRent(item)}>
-                Rent{item?.price ? ` for \$${item.price}` : ''}
+const Item = ({ item, onRent, backdrop }) => {
+  return (
+    <div className="Item">
+      <div className="Item__container" style={{ backgroundImage: `url(${backdrop})` }}>
+        <div className="Item__overlay">
+          <div className="title">{item?.original_title ?? 'Unknown Title'}</div>
+          <div className="rating">{item?.vote_average ?? 0} / 10</div>
+          <div style={{ flex: '1 auto' }} />
+          { onRent ?
+            <>
+              {!!item?.price &&
+                <div className='Item__price'>${item.price}</div>
+              }
+              <div style={{ flex: '1 auto' }} />
+              <div className="Item__button" onClick={() => onRent(item)}>
+                Rent
               </div>
-          </div>
+            </> :
+            <>
+              <div className="Item__button Item__button--rented">
+                Watch Now
+              </div>
+            </>
           }
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+const CartIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 22" width="16">
+      <path fill="#fff" d="M6.3 22c-.53 0-.99-.2-1.36-.58a1.94 1.94 0 0 1-.56-1.4c0-.55.18-1.02.56-1.4a1.86 1.86 0 0 1 2.71-.01c.38.39.57.86.57 1.4 0 .56-.19 1.03-.56 1.41A1.8 1.8 0 0 1 6.3 22Zm10.68 0c-.54 0-1-.2-1.36-.58a1.94 1.94 0 0 1-.56-1.4c0-.55.18-1.02.56-1.4a1.85 1.85 0 0 1 2.7-.01c.39.39.58.86.58 1.4 0 .56-.19 1.03-.56 1.41-.38.39-.83.58-1.36.58ZM4.9 3.83l2.94 6.28h7.69l3.33-6.28H4.91Zm-.8-1.65h15.72c.57 0 .93.17 1.08.53.16.36.1.76-.14 1.2l-3.6 6.7c-.18.3-.43.57-.75.8-.32.23-.67.35-1.04.35h-8.1L5.8 14.62h13.1v1.65H6.04c-.74 0-1.28-.25-1.61-.77-.33-.51-.33-1.09.01-1.73l1.7-3.25-4.05-8.87H0V0h3.12l1 2.18Zm3.74 7.93h7.69-7.7Z" opacity=".8"/>
+    </svg>
+  );
+}
+
+
+const Cart = ({ cost, titles }) => {
+  return (
+    <div className="Cart">
+      <div className="Cart__container">
+        <div className="Cart__header">
+          <CartIcon />
+          Cart
+        </div>
+        <div className="Cart__list">
+          {titles.map((movie, i) => (
+            <div className="Cart__item" key={i}>
+              <div className="Cart__item-name">{movie.original_title}</div>
+              <div className="Cart__item-price">${movie.price}</div>
+            </div>
+          ))}
+        </div>
+        <div className="Cart__total">
+          <div className="Cart__total-title">Total due:</div>
+          <div className="Cart__total-price">${cost}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const MoviesIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" fill="none" viewBox="0 0 46 53">
+      <path fill="#fff" fillRule="evenodd" d="m34.8 1.47 3.08 7.3L45.3 7.6 44.1 0l-9.3 1.47Zm-3.98.63 3.08 7.3-7.44 1.18-3.06-7.3 7.42-1.18Zm-9.17 9.24-3.06-7.3-7.43 1.18 3.06 7.3 7.43-1.18ZM6.35 5.98c-4.1.65-6.9 4.5-6.26 8.61l.03.16 9.29-1.47-3.06-7.3ZM.1 48.4a3.85 3.85 0 0 0 3.83 3.85H42c2.1 0 3.83-1.73 3.83-3.85V17.64H.09V48.4Zm18.22-21.66 12.27 8.2L18.3 43.1V26.75Z" clipRule="evenodd"/>
+    </svg>
+  );
+};
 
 export default App;
