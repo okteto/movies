f8c1a4202b57f7324261f3a593c54277e1cfbd08
Pablo Chico de Guzman <pchico83@gmail.com>
2023-12-22 17:20:43 +0100

    pre-build images

--- scale.sh
+++ scale.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-for i in {1..5}
+for i in {1..2000}
 do
+  echo $i
   okteto preview deploy --branch test --repository https://github.com/okteto/movies
-  sleep 1
 done
 
 # k delete ns -l preview.okteto.com/scope=personal
